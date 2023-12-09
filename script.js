621f53054984d5b265765897c10bc86b397b3194
lajtg <malgo.wieliczko@gmail.com>
2023-12-09 15:49:30 +0100

    dodanie okiny oka i pokazywanie hasla

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("Hello ALK Students !");
+document.getElementById("pokazhaslo").addEventListener("click", function() {
+    const passwordField = document.getElementById("field2");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        this.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        this.classList.replace("fa-eye-slash", "fa-eye");
+    }
+});
